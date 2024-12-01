b923e2b5e2e78db9ab6f3949642aca8c1db3d130
Markus Neuerbourg <marneuer@gmail.com>
2024-12-01 12:20:37 +0100

    basic structure + day1 solution

---
+++ code/days/day1.py
@@ -0,0 +1,33 @@
+import re
+from days.base import Basesolver
+import numpy as np
+
+class Solver(Basesolver):
+
+    # process input, override if neccessary
+    def process_input(self,input):
+        self.input = np.array(input).T
+    
+    # declare constant stuff for both parts
+    def set_constants(self):
+        return
+
+    # set part for checks and also load part dependent stuff, override if neccessary
+    def set_part(self,part):
+        self.part = part
+
+    def solve_1(self):
+        return sum(np.abs(np.subtract(np.sort(self.input[0]), np.sort(self.input[1]))))
+    
+    def solve_2(self):
+        # left list
+        numbers_left, counts_left = np.unique(self.input[0], return_counts=True)
+        count_dict_left = dict(zip(numbers_left, counts_left))
+        # right list
+        # exclude numbers not in left list
+        right_list = [n for n in self.input[1] if n in self.input[0]]
+        numbers_right, counts_right = np.unique(right_list, return_counts=True)
+        count_dict_right = dict(zip(numbers_right, counts_right))
+
+        # compute similarity score 
+        return (sum([count_dict_left[n]*count_dict_right[n]*n for n in count_dict_right.keys()]))
